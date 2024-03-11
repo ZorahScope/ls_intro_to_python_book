63a2b209da4984c44d48e5cc262dcf2e01a99ad5
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-03-10 21:29:25 -0500

    Complete chapter 1 Data Types

---
+++ chap_1_data_types/exercise_1.py
@@ -0,0 +1,14 @@
+# Identify the data type or class for each of the following values:
+
+'''
+'True'              - String
+False               - Boolean
+(1, 2, 3)           - Tuple
+1.5                 - Float
+[1, 2, 3]           - List
+2                   - Int
+range(5)            - Range
+{1, 2, 3}           - Set
+None                - NoneType
+{'foo': 'bar'}      - Dictionary
+'''
